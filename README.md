cc6f3f301d60fab38cb920510bbf138e5f33a486
burgercoder <burgercoder@gmail.com>
2016-10-08 23:48:28 -0700

    Initial project setup.

---
+++ README.md
@@ -0,0 +1,46 @@
+Third Floor
+============
+
+Third Floor is the next generation doujinshi reader built with Django and Angular 2.
+
+Name
+----
+
+- Third Floor
+- 3F
+- thirdfloor (code only)
+
+Development Environment Setup
+-----------------------------
+
+Pre-requisites
+
+- Python 3.5+
+
+Install `virtualenv` and `virtualenvwrapper`
+
+    pip install virtualenv virtualenvwrapper
+
+Add the following to your `.bashrc` or `.bash_profile`
+
+    export WORKON_HOME=~/.envs
+    source /usr/local/bin/virtualenvwrapper.sh
+
+Setup a Python virtual environment
+
+    $ mkvirtualenv thirdfloor --python=/usr/bin/python3
+    (thirdfloor) $
+
+To reactivate the virtualenv in a new terminal window, use the `workon` command
+
+    $ workon thirdfloor
+    (thirdfloor) $
+
+Install requirements and setup a local database
+
+    (thirdfloor) $ ./scripts/upgrade.sh
+    (thirdfloor) $ ./scripts/autosync.sh
+
+Run the server
+
+    (thirdfloor) $ python manage.py runserver
---
+++ thirdfloor/settings.py
@@ -0,0 +1,63 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = 'yy_rz8&^xn=je$18hix1nzo5e1yrc^rcc$l#7ds9(y=je$ni11'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'thirdfloor.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'thirdfloor.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+
+TIME_ZONE = 'UTC'
+USE_TZ = True
+
+STATIC_URL = '/static/'
