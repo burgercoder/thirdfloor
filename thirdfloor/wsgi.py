18ff2f0e66f049c08bcc82c91d1622a7bf9eab56
burgercoder <burgercoder@gmail.com>
2016-10-09 11:24:36 -0700

    #12 - Remove un-needed type: ignore comments.

--- thirdfloor/wsgi.py
+++ thirdfloor/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-from django.core.wsgi import get_wsgi_application  # type: ignore
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thirdfloor.settings")
 application = get_wsgi_application()  # pylint: disable=invalid-name
