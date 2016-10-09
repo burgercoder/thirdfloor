e82dc2a05f8b3e0b84b68be032ba7b6cb3adc09a
burgercoder <burgercoder@gmail.com>
2016-10-09 01:23:57 -0700

    #7 - Added mypy.

--- thirdfloor/wsgi.py
+++ thirdfloor/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # type: ignore
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thirdfloor.settings")
 application = get_wsgi_application()
